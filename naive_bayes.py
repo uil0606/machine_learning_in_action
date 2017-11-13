209c45db26009f0fc1ca99382f3dcd59b77d49b9
uil0606 <uil0606@outlook.com>
2017-11-13 08:37:03 +0800

    11

--- naive_bayes.py
+++ naive_bayes.py
@@ -78,5 +78,94 @@ def test_NB():
 
 # test_NB()
 
+def text_parse(big_str):
+    import re
+    list_tokens = re.split(r'\w*', big_str)
+    return [tok.low() for tok in list_tokens if len(tok) > 2]  # 小写化长度大于2的单词，作为输入
 
 
+'''
+def spam_test():
+    doc_list = []
+    class_list = []
+    full_text = []
+    for i in range(1, 26):
+        word_list = text_parse(open('email/spam/%d.txt' % i).read())
+        doc_list.append(word_list)
+        full_text.extend(word_list)
+        class_list.append(1)
+        word_list = text_parse(open('email/ham/%d.txt' % i).read())
+        doc_list.append(word_list)
+        full_text.extend(word_list)
+        class_list.append(0)
+    vocal_list = create_vocal_list(doc_list)
+    training_set = range(50)
+    test_set = []
+    for i in range(10):
+        rand_index = int(random.uniform(0, len(training_set)))
+        test_set.append(training_set[rand_index])  # 生成测试集
+        del (training_set[rand_index])
+    train_mat = []
+    train_classes = []
+    for doc_index in training_set:
+        train_mat.append(word_to_vec(vocal_list, doc_list[doc_index]))
+        train_classes.append(class_list[doc_index])
+    p0_v, p1_v, p_spam = train_NB0(array(train_mat), array(train_classes))
+    err_count = 0
+    for doc_index in test_set:
+        word_vec = word_to_vec(vocal_list, doc_list[doc_index])
+        if classify_NB(array(word_vec), p0_v, p1_v, p_spam) != class_list[doc_index]:
+            err_count += 1
+    print('the error rate is: ', float(err_count) / len(test_set))
+'''
+
+
+def cal_most_freq(vocal_list, full_text):
+    import operator
+    freq_dict = {}
+    for token in vocal_list:
+        freq_dict[token] += full_text.count(token)
+    sorted_freq = sorted(freq_dict.items(), key=operator.itemgetter(1), reverse=True)
+    return sorted_freq[:30]
+
+
+def local_words(feed1, feed0):
+    # 利用rss程序库获取文本数据源
+    import feedparser
+    doc_list = []
+    class_list = []
+    full_text = []
+    min_len = min(len(feed1['entries']), len(feed0['entries']))
+    for i in range(min_len):
+        word_list = text_parse(feed1['entries'][i]['summary'])
+        doc_list.append(word_list)
+        full_text.extend(word_list)
+        class_list.append(1)
+        word_list = text_parse(feed0['entries'][i]['summary'])
+        doc_list.append(word_list)
+        full_text.extend(word_list)
+        class_list.append(0)
+    vocal_list = create_vocal_list(doc_list)
+    top_words = cal_most_freq(vocal_list, full_text)
+    for pairw in top_words:
+        if pairw[0] in vocal_list:
+            vocal_list.remove(pairw[0])
+    training_set = range(2 * min_len)
+    test_set = []
+    for i in range(20):
+        rand_index = int(random.uniform(0, len(training_set)))
+        test_set.append(training_set[rand_index])
+        del (training_set[rand_index])
+    train_mat = []
+    train_classes = []
+    for doc_index in training_set:
+        train_mat.append(word_to_vec(vocal_list, doc_list[doc_index]))
+        train_classes.append(class_list[doc_index])
+    p0_v, p1_v, p_spam = train_NB0(array(train_mat, array(train_classes)))
+    err_count = 0
+    for doc_index in test_set:
+        word_vec = word_to_vec(vocal_list, doc_list[doc_index])  # doc_list?
+        if classify_NB(array(word_vec), p0_v, p1_v, p_spam) != class_list[doc_index]:
+            err_count += 1
+    print('the error rate is: ', float(err_count) / len(test_set))
+return vocal_list, p0_v, p1_v
